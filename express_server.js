9524fe0d2e4a323b3156c01e0595d3fcec85b0e3
ZuhraB <zuhra.bakhshi.com>
2020-08-31 22:41:39 +0000

    added the form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +10,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let str = '';
+  const CHAR_SET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++){
+    str += CHAR_SET.charAt(Math.floor(Math.random() * CHAR_SET.length))
+  }
+  return str;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,6 +34,13 @@ app.get("/urls", (req,res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 })
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
