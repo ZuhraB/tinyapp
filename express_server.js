60a174ce93f98ca1a00ea46eedfa272956c912e6
ZuhraB <zuhra.bakhshi.com>
2020-08-31 21:31:29 +0000

    added the shortUrl page

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/urls", (req,res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 })
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 
 app.listen(PORT, () => {
